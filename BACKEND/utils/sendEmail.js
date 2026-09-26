44d9a98e415b689dbfa51984dea074fb27b44e33
kparitosh07 <kushwahaparitosh7@gmail.com>
2026-09-26 15:26:18 +0530

    still get the error of connection

--- BACKEND/utils/sendEmail.js
+++ BACKEND/utils/sendEmail.js
@@ -1,13 +1,9 @@
 import nodemailer from "nodemailer";
-import dns from "node:dns";
-
-dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
